a06cf5a015fe082ce23a25d70f2e775ca519e7c1
Sourabh verma <150373765+sourabh1211@users.noreply.github.com>
2025-08-01 01:14:39 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,11 @@ var express = require('express');
 const { signUp, login, createProj, saveProject, getProjects, getProject, deleteProject, editProject } = require('../controllers/userController');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post("/signUp", signUp); // signUp is the controller function
+router.post("/signUp", signUp);
 router.post("/login", login); 
 router.post("/createProj", createProj); 
 router.post("/saveProject", saveProject); 
